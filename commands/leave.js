2875923fd2094815d9a56f4c09456e60643b87cd
PanIntegralus <pablikoxyt@gmail.com>
2022-12-07 20:24:52 +0100

    temporary comment

--- commands/leave.js
+++ commands/leave.js
@@ -1,6 +1,12 @@
 exports.run = async (client, message, args) => {
     const queue = client.player.getQueue(message.guild);
 
+    // if (message.member.voice.channel != client.player.channel)
+    // {
+    //     await message.reply("`❌` You must be in the same channel as the bot.");
+    //     return;
+    // };
+
     await queue.destroy();
     await message.reply("`✅` Player has disconnected.")
 };
